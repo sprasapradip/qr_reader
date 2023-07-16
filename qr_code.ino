10ff771cd6325f0171c6f8864cfa176681b6bdcc
Pradip Subedi <114067364+sprasapradip@users.noreply.github.com>
2023-07-16 23:05:41 +0545

    Update qr_code.ino

--- qr_code.ino
+++ qr_code.ino
@@ -42,6 +42,37 @@ void setup() {
   Serial.begin(9600);
   qrSerial.begin(9600);
 }
+void loop() {
+  if (qrSerial.available()) {
+    char receivedChar = qrSerial.read();
+    if (receivedChar == '\n') {
+      if (studentName.length() > 0 && rollNumber.length() > 0) {
+        sendToHTML(studentName, rollNumber);
+        studentName = "";
+        rollNumber = "";
+      }
+    } else {
+      studentName += receivedChar;
+    }
+  }
+
+  // Additional feature: Check for serial input from the computer
+  if (Serial.available()) {
+    char inputChar = Serial.read();
+    if (inputChar == 'r' || inputChar == 'R') {
+      resetData();
+    }
+  }
+}
+
+void sendToHTML(String name, String roll) {
+  Serial.print("<html><body>");
+  Serial.print("<h1>Student Information:</h1>");
+  Serial.print("<p>Name: " + name + "</p>");
+  Serial.print("<p>Roll Number: " + roll + "</p>");
+  Serial.print("</body></html>");
+}
+
 
 
 
